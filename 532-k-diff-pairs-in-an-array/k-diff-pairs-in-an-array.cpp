ed00b7abb274356ce72eabc7f5660bc2de8ff347
itsAJAY0017 <141677772+itsAJAY0017@users.noreply.github.com>
2024-08-14 00:25:04 +0530

    Time: 18 ms (35.53%) | Memory: 15.9 MB (58.64%) - LeetSync

---
+++ 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    //Binary Search Method
+    // bool BS(vector<int>&arr,int s,int x){
+    //   int e=arr.size()-1;
+    //   while(s<=e){
+    //     int mid=(s+e)/2;
+    //     if(arr[mid]==x){
+    //         return true;
+    //     }
+    //     else if(x>arr[mid]){
+    //         s=mid+1;
+    //     }
+    //     else{
+    //         e=mid-1;
+    //     }
+    //   }
+    //   return false;
+    // }
+    // int findPairs(vector<int>& arr, int k){
+    //     int n=arr.size();
+    //     set<pair<int,int>>ans;
+    //     sort(arr.begin(),arr.end());
+    //     //Binary search if a[i] then a[i]+k==a[j] is present or not
+    //     for(int i=0;i<n;i++){
+    //       if(BS(arr,i+1,arr[i]+k)){
+    //          ans.insert({arr[i],arr[i]+k});
+    //       }
+    //     }
+    //     return ans.size();
+    // }
+
+
+    
+    int findPairs(vector<int>& nums, int k) {
+     sort(nums.begin(),nums.end());
+     set<pair<int,int>>ans;
+     int i=0,j=1;
+     while(j<nums.size()){
+       int diff=nums[j]-nums[i];
+       if(diff==k){
+         ans.insert({nums[i],nums[j]});
+         i++,j++;
+       }
+       else{
+         if(diff>k) i++;
+         else j++;
+         if(i==j) j++;
+       }
+     }
+     return ans.size();
+    }
+};
