d0e8d685b4a4f74a08208f9af19f271761fffd7d
itsAJAY0017 <141677772+itsAJAY0017@users.noreply.github.com>
2024-08-15 01:03:08 +0530

    Time: 49 ms (64.46%) | Memory: 20.9 MB (54.07%) - LeetSync

--- 524-longest-word-in-dictionary-through-deleting/longest-word-in-dictionary-through-deleting.cpp
+++ 524-longest-word-in-dictionary-through-deleting/longest-word-in-dictionary-through-deleting.cpp
@@ -1,47 +1,25 @@
-// class Solution {
-// public:
-//     string findLongestWord(string s, vector<string>& dictionary) {
-//         string ans="";
-//         sort(s.begin(),s.end());
-//         for(int it=0;it<dictionary.size();it++){
-//             string temp=dictionary[it];
-//             sort(temp.begin(),temp.end());
-//             int i=0,j=0;
-
-//             while(i<s.size() && j<temp.size()){
-//                 if(s[i]==temp[j]){
-//                     i++,j++;
-//                 }
-//                 else i++;
-//             }
-//             if(j==temp.size()){
-//                 if(temp.size()>ans.size()) ans=dictionary[it];
-//                 // if(temp.size()==ans.size()){
-//                 //     //maintain lexography bcz yha glti hogi
-//                 //     if(temp<ans) ans=dictionary[it];
-//                 // }
-//             }
-//         }
-//         return ans;
-//     }
-// };
 class Solution {
 public:
     string findLongestWord(string s, vector<string>& dictionary) {
-        string ans = "";
-        for (string& a : dictionary)
-            if (check(s, a) && (ans.size() < a.size() || (ans.size() == a.size() && a < ans)))
-                ans = a;
-        return ans;
-    }
+        string ans="";
+        for(int it=0;it<dictionary.size();it++){
+            string temp=dictionary[it];
+            int i=0,j=0;
 
-    bool check(string& a, string& b) {
-        int m = a.size(), n = b.size();
-        int i = 0, j = 0;
-        while (i < m && j < n) {
-            if (a[i] == b[j]) ++j;
-            ++i;
+            while(i<s.size() && j<temp.size()){
+                if(s[i]==temp[j]){
+                    i++,j++;
+                }
+                else i++;
+            }
+            if(j==temp.size()){
+                if(temp.size()>ans.size()) ans=dictionary[it];
+                if(temp.size()==ans.size()){
+                    //maintain lexography bcz yha glti hogi
+                    if(temp<ans) ans=dictionary[it];
+                }
+            }
         }
-        return j == n;
+        return ans;
     }
 };
