0587249f2691207f10034a0ee45acd9a000686e9
itsAJAY0017 <141677772+itsAJAY0017@users.noreply.github.com>
2024-08-26 14:41:04 +0530

    Time: 7 ms (98.22%) | Memory: 15.1 MB (78.41%) - LeetSync

---
+++ 775-n-ary-tree-preorder-traversal/n-ary-tree-preorder-traversal.cpp
@@ -0,0 +1,35 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    void solve(Node*&root,vector<int>&arr){
+        if(root==NULL) return;
+        arr.push_back(root->val);
+        for(auto child:root->children){
+            solve(child,arr);
+        }
+    }
+    vector<int> preorder(Node* root) {
+        vector<int>arr;
+        solve(root,arr);
+        return arr;
+    }
+};
