62ea522ac6d16d338df99cfe112707fcd6332cba
itsAJAY0017 <141677772+itsAJAY0017@users.noreply.github.com>
2024-09-11 03:44:17 +0530

    Time: 22 ms (41.06%) | Memory: 13.8 MB (61.56%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.cpp
+++ 227-basic-calculator-ii/basic-calculator-ii.cpp
@@ -3,10 +3,10 @@
 class Solution {
 public:
     int calculate(string s) {
-        stack<int> operands;  // Stack to store intermediate results
-        int result = 0;       // Final result
-        int num = 0;          // Current number being processed
-        char op = '+';        // Current operator, initialized to '+'
+        stack<int> operands; 
+        int result = 0;    
+        int num = 0;         
+        char op = '+'; 
         
         for (int i = 0; i < s.size(); ++i) {
             char c = s[i];
@@ -19,17 +19,17 @@ public:
             // If the current character is an operator or we reach the end of the string
             if (!isdigit(c) && !isspace(c) || i == s.size() - 1) {
                 if (op == '+') {
-                    operands.push(num);  // Push the current number to the stack
+                    operands.push(num);  
                 } else if (op == '-') {
-                    operands.push(-num); // Push the negative number to the stack
+                    operands.push(-num); 
                 } else if (op == '*') {
                     int top = operands.top();
                     operands.pop();
-                    operands.push(top * num);  // Multiply the top number with the current number
+                    operands.push(top * num); 
                 } else if (op == '/') {
                     int top = operands.top();
                     operands.pop();
-                    operands.push(top / num);  // Divide the top number by the current number
+                    operands.push(top / num); 
                 }
                 
                 // Update the operator and reset the number
@@ -38,7 +38,6 @@ public:
             }
         }
 
-        // Sum up all values in the stack to get the final result
         while (!operands.empty()) {
             result += operands.top();
             operands.pop();
