1399070dbe426c6f73d1c0ec749ae77bb724b2df
itsAJAY0017 <141677772+itsAJAY0017@users.noreply.github.com>
2024-09-13 10:50:19 +0530

    Time: 69 ms (35.59%) | Memory: 44.6 MB (27.66%) - LeetSync

--- 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.cpp
+++ 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.cpp
@@ -1,21 +1,38 @@
 class Solution {
 public:
     vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
+        // vector<int>ans;
+        // for(auto v:queries){
+        //     int i=v[0];
+        //     int j=v[1];
+        //     if(i==j){
+        //         ans.push_back(arr[i]);
+        //         continue;
+        //     } 
+
+        //     int x=arr[i];
+        //     for(i=i+1;i<=j;i++){
+        //         x=x^arr[i];
+        //     }
+        //     ans.push_back(x);
+        // }
+        // return ans;
+
         vector<int>ans;
+        //cumulative XOR
+        for(int i=1;i<arr.size();i++){
+            arr[i]=arr[i]^arr[i-1];
+        }
+
         for(auto v:queries){
             int i=v[0];
             int j=v[1];
-            if(i==j){
-                ans.push_back(arr[i]);
-                continue;
-            } 
-
-            int x=arr[i];
-            for(i=i+1;i<=j;i++){
-                x=x^arr[i];
-            }
-            ans.push_back(x);
+            
+            int val=arr[j]^(i==0?0:arr[i-1]);
+            
+            ans.push_back(val);
         }
         return ans;
+
     }
 };
