37e55372c7e0b09ed0b3fed3e0d45a963a2b6106
itsAJAY0017 <141677772+itsAJAY0017@users.noreply.github.com>
2024-09-15 14:55:43 +0530

    Time: 84 ms (36.51%) | Memory: 19 MB (32.79%) - LeetSync

--- 1473-find-the-longest-substring-containing-vowels-in-even-counts/find-the-longest-substring-containing-vowels-in-even-counts.cpp
+++ 1473-find-the-longest-substring-containing-vowels-in-even-counts/find-the-longest-substring-containing-vowels-in-even-counts.cpp
@@ -1,40 +1,25 @@
 class Solution {
 public:
     int findTheLongestSubstring(string s) {
-        unordered_map<string, int> mp;
-    
-        // Initialize an array to track the count of each vowel
-        vector<int> vowelCount(5, 0); // Count of 'a', 'e', 'i', 'o', 'u'
-
-        string currentState = "00000";  // Initial state where all vowels have even counts
-        mp[currentState] = -1;
-        
-        int maxLength = 0;
-
-        for (int i = 0; i < s.length(); ++i) {
-            // Update count for the vowel encountered
-            if (s[i] == 'a')      vowelCount[0] = (vowelCount[0] + 1) % 2;
-            else if (s[i] == 'e') vowelCount[1] = (vowelCount[1] + 1) % 2;
-            else if (s[i] == 'i') vowelCount[2] = (vowelCount[2] + 1) % 2;
-            else if (s[i] == 'o') vowelCount[3] = (vowelCount[3] + 1) % 2;
-            else if (s[i] == 'u') vowelCount[4] = (vowelCount[4] + 1) % 2;
-
-            // Create a string representing the current parity state of vowels
-            currentState = "";
-            for (int j = 0; j < 5; ++j) {
-                currentState += to_string(vowelCount[j]);
-            }
-
-            // Check if we've seen this state before
-            if (mp.find(currentState) != mp.end()) {
-                // Calculate the length of the substring
-                maxLength = max(maxLength, i - mp[currentState]);
-            } else {
-                // Store the first occurrence of this state
-                mp[currentState] = i;
-            }
+        unordered_map<int, int> mp;
+        int mask = 0;
+        mp[mask] = -1;
+        int maxL = 0;
+        for(int i = 0; i<s.length(); i++) {
+            int val = 0;
+            if (s[i] == 'a')      val  = (1 << 0);
+            else if (s[i] == 'e') val = (1 << 1);
+            else if (s[i] == 'i') val = (1 << 2);
+            else if (s[i] == 'o') val = (1 << 3);
+            else if (s[i] == 'u') val = (1 << 4);
+            
+            mask = mask^val; //xor nikala
+            
+            if(!mp.count(mask)) //agar past me nahi dekha to map me daaldo
+                mp[mask] = i;
+            
+            maxL = max(maxL, i - mp[mask]); //maxL nikaal lo
         }
-
-        return maxLength;
+        return maxL;
     }
 };
