bd1b54b8cb96901b6b8ee535c5ad70e634599dd0
itsAJAY0017 <141677772+itsAJAY0017@users.noreply.github.com>
2024-09-17 14:05:07 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (28.22%) - LeetSync

---
+++ 920-uncommon-words-from-two-sentences/uncommon-words-from-two-sentences.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void countWords( string &sentence, unordered_map<string, int> &wordCount) {
+        stringstream ss(sentence);//stl use to split the string
+        string word;
+        while (ss >> word) {
+          // cout<<word<<endl;
+           wordCount[word]++;
+         }
+    }
+    vector<string> uncommonFromSentences(string s1, string s2) {
+         unordered_map<string, int> wordCount;
+         countWords(s1, wordCount);
+         countWords(s2, wordCount);
+    
+         vector<string> result;
+    
+         for (auto x : wordCount) {
+            if (x.second == 1) {
+               result.push_back(x.first);
+            }
+         }
+         return result;
+    }
+};
