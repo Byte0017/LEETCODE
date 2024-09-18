b0aead713846ea092611ebad9bd06c7e0335addb
itsAJAY0017 <141677772+itsAJAY0017@users.noreply.github.com>
2024-09-18 13:15:41 +0530

    Time: 7 ms (61.31%) | Memory: 15.8 MB (63.72%) - LeetSync

--- 179-largest-number/largest-number.cpp
+++ 179-largest-number/largest-number.cpp
@@ -1,15 +1,4 @@
-// class Solution {
-// public:
-//     string largestNumber(vector<int>& nums) {
-//         string ans="";
-//         for(int i=0;i<nums.size();i++){
-//             string s=to_string(nums[i]);
-//             ans=ans+s;
-//         }
-//         sort(ans.rbegin(),ans.rend());
-//         return ans;  
-//     }
-// };
+
 class Solution {
  public:
   string largestNumber(vector<int>& nums) {
@@ -18,8 +7,9 @@ class Solution {
     ranges::sort(nums, [](int a, int b) {
       return to_string(a) + to_string(b) > to_string(b) + to_string(a);
     });
+    // for(auto x:nums) cout<<x<<" ";
 
-    for (const int num : nums)
+    for (int num : nums)
       ans += to_string(num);
 
     return ans[0] == '0' ? "0" : ans;
