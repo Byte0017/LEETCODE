d531a46ad56dff4e85b8d6c15afcbdc9a3a5c313
itsAJAY0017 <141677772+itsAJAY0017@users.noreply.github.com>
2024-09-20 20:00:03 +0530

    Time: 15 ms (11.01%) | Memory: 9.6 MB (90.93%) - LeetSync

--- 214-shortest-palindrome/shortest-palindrome.cpp
+++ 214-shortest-palindrome/shortest-palindrome.cpp
@@ -23,35 +23,61 @@
 // };
 
 //M:2 Efficient approach
+// class Solution {
+// public:
+//     string shortestPalindrome(string s) {
+//         string temp="";
+//         string t1,t2;
+
+//         int i=0,j=s.size()-1;
+
+//         while(i<j){
+//             t1.push_back(s[i]);
+//             t2.push_back(s[j]);
+
+//             if(t1==t2) {
+//                 i++,j--;
+//                 continue;
+//             }
+//             else{
+//                while(t1!=t2){
+//                    temp+=t2[0];
+//                    //pop() in t1 from back and in t2 from front
+//                    t1.pop_back();
+//                    t2.erase(0, 1);
+//                }
+//                //update i and j
+//                j--;
+//                i=t2.size();
+//             }
+//         }
+//         temp=temp+s;
+//         return temp;
+//     }
+// };
+
+//M3
 class Solution {
 public:
     string shortestPalindrome(string s) {
-        string temp="";
-        string t1,t2;
+       string rev=s;
+       reverse(rev.begin(),rev.end());
 
-        int i=0,j=s.size()-1;
+       //    int index=-1;
+       int n=s.size();
+       for(int i=0;i<n;i++){
+        //    string str1=s.substr(0,n-i);
+        //    string str2=rev.substr(i,n-i);
 
-        while(i<j){
-            t1.push_back(s[i]);
-            t2.push_back(s[j]);
+        //    if(str1==str2){
+        //       index=i-1; 
+        //       break;
+        //    }
 
-            if(t1==t2) {
-                i++,j--;
-                continue;
-            }
-            else{
-               while(t1!=t2){
-                   temp+=t2[0];
-                   //pop() in t1 from back and in t2 from front
-                   t1.pop_back();
-                   t2.erase(0, 1);
-               }
-               //update i and j
-               j--;
-               i=t2.size();
-            }
-        }
-        temp=temp+s;
-        return temp;
+              if(!memcmp(s.c_str(),rev.c_str()+i,n-i)){
+                 return rev.substr(0,i)+s;
+              }  
+       }
+       return rev+s;
     }
 };
