e4f6258f57a02c3951ec186e38471c2261b4cb5e
itsAJAY0017 <141677772+itsAJAY0017@users.noreply.github.com>
2024-09-20 21:21:00 +0530

    Time: 5 ms (74.26%) | Memory: 11.5 MB (73.86%) - LeetSync

--- 214-shortest-palindrome/shortest-palindrome.cpp
+++ 214-shortest-palindrome/shortest-palindrome.cpp
@@ -1,83 +1,49 @@
-// class Solution {
-// public:
-//     bool check(string& s,int j){
-//         int i=0;
-//         while(i<=j){
-//             if(s[i]!=s[j]) return false;
-//             i++,j--;
-//         }
-//         return true;
-//     }
-//     string shortestPalindrome(string s) {
-//         int n=s.size();
-//         string temp="";
-//         for(int i=n-1;i>=0;i--){
-//             if(!check(s,i)){
-//                 temp=temp+s[i];
-//             }
-//             else break;
-//         }
-//         temp=temp+s;
-//         return temp;
-//     }
-// };
-
-//M:2 Efficient approach
-// class Solution {
-// public:
-//     string shortestPalindrome(string s) {
-//         string temp="";
-//         string t1,t2;
-
-//         int i=0,j=s.size()-1;
-
-//         while(i<j){
-//             t1.push_back(s[i]);
-//             t2.push_back(s[j]);
-
-//             if(t1==t2) {
-//                 i++,j--;
-//                 continue;
-//             }
-//             else{
-//                while(t1!=t2){
-//                    temp+=t2[0];
-//                    //pop() in t1 from back and in t2 from front
-//                    t1.pop_back();
-//                    t2.erase(0, 1);
-//                }
-//                //update i and j
-//                j--;
-//                i=t2.size();
-//             }
-//         }
-//         temp=temp+s;
-//         return temp;
-//     }
-// };
-
-//M3
 class Solution {
 public:
-    string shortestPalindrome(string s) {
-       string rev=s;
-       reverse(rev.begin(),rev.end());
-
-       //    int index=-1;
-       int n=s.size();
-       for(int i=0;i<n;i++){
-        //    string str1=s.substr(0,n-i);
-        //    string str2=rev.substr(i,n-i);
-
-        //    if(str1==str2){
-        //       index=i-1; 
-        //       break;
-        //    }
-
-              if(!memcmp(s.c_str(),rev.c_str()+i,n-i)){
-                 return rev.substr(0,i)+s;
-              }  
-       }
-       return rev+s;
+    
+// Function to calculate LPS array (used in KMP)
+void calculateLPSArray(const string& s, vector<int>& lps) {
+    int length = 0;
+    int i = 1;
+    lps[0] = 0;
+    
+    while (i < s.length()) {
+        if (s[i] == s[length]) {
+            length++;
+            lps[i] = length;
+            i++;
+        } else {
+            if (length != 0) {
+                length = lps[length - 1];
+            } else {
+                lps[i] = 0;
+                i++;
+            }
+        }
     }
+}
+
+// Function to find the shortest palindrome using KMP algorithm
+string shortestPalindrome(string s) {
+    if (s.empty()) return s;
+
+    // Create new string: original + "#" + reverse(original)
+    string rev_s = s;
+    reverse(rev_s.begin(), rev_s.end());
+    string s_new = s + "#" + rev_s;
+
+    // Create LPS array for the new string
+    vector<int> lps(s_new.length(), 0);
+    calculateLPSArray(s_new, lps);
+
+    // Length of the longest palindrome prefix
+    int longest_palindrome_prefix_len = lps.back();
+
+    // Add the non-palindromic suffix of original string reversed at the front
+    string suffix = s.substr(longest_palindrome_prefix_len);
+    reverse(suffix.begin(), suffix.end());
+
+    return suffix + s;
+}
+
 };
