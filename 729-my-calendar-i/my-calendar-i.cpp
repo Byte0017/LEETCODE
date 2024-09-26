ee2af9c20d0e48975cf181ad87b396c46c6e27de
itsAJAY0017 <141677772+itsAJAY0017@users.noreply.github.com>
2024-09-26 18:01:49 +0530

    Time: 73 ms (76.51%) | Memory: 42.7 MB (59.10%) - LeetSync

---
+++ 729-my-calendar-i/my-calendar-i.cpp
@@ -0,0 +1,14 @@
+class MyCalendar {
+public:
+    set<pair<int, int>> st;
+    MyCalendar() { }
+    bool book(int start, int end) {
+        auto it = st.lower_bound({start, end}); 
+        if(it != st.end() && end > it->first)
+            return false;
+        if(it != st.begin() && start < ((--it)->second))
+            return false;
+        st.insert({start, end});
+        return true;
+    }
+};
