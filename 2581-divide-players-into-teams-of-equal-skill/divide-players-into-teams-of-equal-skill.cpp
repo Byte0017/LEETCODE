2e99ed678519b93cc657d9bd0cee554252557a21
itsAJAY0017 <141677772+itsAJAY0017@users.noreply.github.com>
2024-10-04 12:43:04 +0530

    Time: 44 ms (99.00%) | Memory: 56.5 MB (23.40%) - LeetSync

--- 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.cpp
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     long long dividePlayers(vector<int>& skill) {
         long long int total=accumulate(skill.begin(),skill.end(),0);
-        int size=skill.size();
-        if(total%(size/2)) return -1;
+        int n=skill.size();
+        if(total%(n/2)) return -1;
 
-        int pairSum=total/(size/2);
+        int pairSum=total/(n/2);
         long long int productSum=0;
         
         sort(skill.begin(),skill.end());
         int i=0;
-        int j=size-1;
+        int j=n-1;
         while(i<j){
             if(skill[i]+skill[j]==pairSum){
                 productSum+=skill[i]*skill[j];
