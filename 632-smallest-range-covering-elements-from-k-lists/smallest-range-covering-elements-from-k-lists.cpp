fa7f818aa0082487f38c1cf2dd75147deb8e38d7
itsAJAY0017 <141677772+itsAJAY0017@users.noreply.github.com>
2024-10-14 01:21:48 +0530

    Time: 93 ms (10.09%) | Memory: 27.3 MB (10.22%) - LeetSync

--- 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.cpp
+++ 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.cpp
@@ -30,7 +30,6 @@ public:
                 range[1] = maxEl;
             }
 
-            //
             //check khi out of bount to nhi ho gya
             if(minEl_idx+1 < nums[list_idx].size()) {
                 pq.push({nums[list_idx][minEl_idx+1], list_idx, minEl_idx+1});
@@ -44,40 +43,4 @@ public:
 };
 
 
-// class Solution {
-// public:
-//     vector<int> smallestRange(vector<vector<int>>& nums) {
-//         int k=nums.size();  
-//         vector<int>arrIdx(k,0);//this stroe the index of every row vector in nums
-//         vector<int>range={-100000 , 100000};//store min and max
-
-//         while(true){
-//             int minEl=INT_MAX;
-//             int maxEl=INT_MIN;
-//             int idx_minEl=0;
-
-//             for(int i=0;i<k;i++){  // i tells which list is taken
-//                 int x=nums[i][arrIdx[i]];
-//                 if(x<minEl){//update minEl and index ki what is idx for minEl
-//                     minEl=x;
-//                     idx_minEl=i;
-//                 }
-//                 maxEl=max(maxEl,nums[i][arrIdx[i]]);
-//             }
-//             //update range
-//             if(range[1]-range[0]>maxEl-minEl){
-//                 range[0]=minEl;
-//                 range[1]=maxEl;
-//             }
-
-//             //to minimize range update minimum idx in arrIdx
-//             arrIdx[idx_minEl]++;
-//             if(arrIdx[idx_minEl] >= nums[idx_minEl].size()){
-//                 //if idx is out of bound of any list
-//                 break;
-//             }
-//         }
-//         return range;
-//     }
-// };
 
